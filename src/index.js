513a743f7663102e630fa88d4763e96cda702bfa
Alex Navarro <wosorio@hightech-corp.com>
2021-07-24 14:44:50 -0600

    setting up the redux store as globally in the index.js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import store from './application/redux/store';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'react-toastify/dist/ReactToastify.css';
@@ -9,7 +11,9 @@ import App from './App';
 
 ReactDOM.render(
     <React.StrictMode>
-        <App />
+        <Provider store={store}>
+            <App />
+        </Provider>
     </React.StrictMode>,
     document.getElementById('root')
 );
