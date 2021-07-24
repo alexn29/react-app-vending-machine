428ada20ccb9baa55e8e7df191dbb13cd9533bb8
Alex Navarro <wosorio@hightech-corp.com>
2021-07-24 13:44:31 -0600

    importing Products component into Home page

--- src/application/components/pages/Home.js
+++ src/application/components/pages/Home.js
@@ -1,9 +1,10 @@
 import React from 'react'
+import Products from '../products/Products'
 
 const Home = () => {
     return (
         <div>
-            <h1>Home</h1>
+            <Products />
         </div>
     )
 }
