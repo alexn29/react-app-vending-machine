933ee005cb99545c6734d8dc0e0035c07403f9a3
Alex Navarro <wosorio@hightech-corp.com>
2021-07-24 13:11:27 -0600

    defining router for home and order pages

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
 import React from 'react'
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+
+import Home from './application/components/pages/Home';
+import Orders from './application/components/pages/Orders';
+
+import NavBar from './application/components/layout/NavBar';
 
 const App = () => {
     return (
-        <div>
-            <h1>Hello world</h1>
-        </div>
+        <BrowserRouter>
+            <NavBar />
+            <Switch>
+                <Route exact path="/" component={Home} />
+                <Route exact path="/orders" component={Orders} />
+            </Switch>
+        </BrowserRouter>
     )
 }
 
