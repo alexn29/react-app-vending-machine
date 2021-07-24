495256b0ecdaf1b88e43292c81aca8a78fedd256
Alex Navarro <wosorio@hightech-corp.com>
2021-07-24 13:43:55 -0600

    rendering the ToastContainer in the App component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { BrowserRouter, Switch, Route } from "react-router-dom";
+import { ToastContainer, Zoom } from 'react-toastify';
 
 import Home from './application/components/pages/Home';
 import Orders from './application/components/pages/Orders';
@@ -10,6 +11,10 @@ const App = () => {
     return (
         <BrowserRouter>
             <NavBar />
+            <ToastContainer
+                position="top-center"
+                transition={Zoom}
+            />
             <Switch>
                 <Route exact path="/" component={Home} />
                 <Route exact path="/orders" component={Orders} />
