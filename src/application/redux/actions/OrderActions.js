1a2d865e0c2d0de62851c49a2d861d1268e30105
Alex Navarro <wosorio@hightech-corp.com>
2021-07-25 00:31:31 -0600

    added an action to change the preparation time every second, also added an action to delete orders

--- src/application/redux/actions/OrderActions.js
+++ src/application/redux/actions/OrderActions.js
@@ -6,11 +6,33 @@ export const createOrder = (product) => async (dispatch, getState) => {
     let data = {
         ...product,
         orderId: uuid(),
-        status: 'Pending'
+        status: 'Pending',
+        createdAt: new Date().toLocaleString(),
     }
 
     dispatch({
         type: types.ADD_ORDER,
         payload: data
     });
+}
+
+export const updatePreparationTime = () => async (dispatch, getState) => {
+    const { order } = getState();
+    const products = [...order.products];
+
+    products.length > 0 && products.map(product => {
+        product.preparation_time = product.preparation_time > 0 ? product.preparation_time - 1 : 0;
+        return product;
+    })
+    
+    dispatch({
+        type: types.UPDATE_PREPARATION_TIME_ORDER,
+        payload: products
+    })
+}
+
+export const deleteOrders = () => async (dispatch, getState) => {
+    dispatch({
+        type: types.DELETE_ORDERS,
+    })
 }
