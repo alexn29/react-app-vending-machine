d636cc1d6566b55a00955393360ee50f0ef40769
Alex Navarro <wosorio@hightech-corp.com>
2021-07-25 00:30:36 -0600

    added new cases in the orderReducer to delete orders and change the status order

--- src/application/redux/reducers/OrderReducer.js
+++ src/application/redux/reducers/OrderReducer.js
@@ -11,6 +11,26 @@ const OrderReducer = (state = initState, action) => {
                 ...state,
                 products: [...state.products, action.payload]
             }
+        
+        case types.UPDATE_PREPARATION_TIME_ORDER:
+            
+            action.payload.length > 0 && action.payload.map(product => {
+                if (product.preparation_time === 0 && product.status === 'Pending') {
+                    product.status = 'Dispatched'
+                }
+                return product;
+            })
+
+            return {
+                ...state,
+                products: action.payload
+            }
+        
+        case types.DELETE_ORDERS:
+            return {
+                products: []
+            }
+
         default:
             return state;
     }
