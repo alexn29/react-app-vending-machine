3ae6b083a1f50c0b1836515de52e4c8d9d520e83
Alex Navarro <wosorio@hightech-corp.com>
2021-07-25 00:28:38 -0600

    created the actions types DELETE_ORDERS and UPDATE_PREPARATION_TIME_ORDER

--- src/application/redux/types/index.js
+++ src/application/redux/types/index.js
@@ -1,1 +1,3 @@
 export const ADD_ORDER = '@order/add_order';
+export const UPDATE_PREPARATION_TIME_ORDER = '@order/update_preparation_time_order';
+export const DELETE_ORDERS = '@order/delete_orders';
